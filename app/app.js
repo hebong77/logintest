0bfcdf8de878d0d72d146cdca136d5b072dfba67
hebong77 <hebong2@gmail.com>
2023-01-31 19:55:22 +0900

    javascript 추가

--- app/app.js
+++ app/app.js
@@ -32,6 +32,8 @@ const home = require("./src/routes/home");
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`))
+
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
 
 
